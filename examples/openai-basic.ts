1c8c83bb99ad810ffbe3826ae74982749ec8e526
Arne Bahlo <arne@axiom.co>
2023-03-30 10:39:35 +0200

    Add dotenv for examples

--- examples/openai-basic.ts
+++ examples/openai-basic.ts
@@ -1,7 +1,10 @@
+import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from "openai";
 import withAxiom from "axiom-ai/openai";
 
 (async function() {
+  dotenv.config()
+
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
